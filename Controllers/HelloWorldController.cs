6fea9621b6a734dfbb3e141232285ba792ae4674
dev3 <dev3@DESKTOP-BSHTT23>
2022-10-20 14:39:21 +0200

    initial commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers {
+  public class HelloWorldController : Controller {
+    public IActionResult Index() {
+      return View();
+    }
+
+    public IActionResult Welcome(string name, int numtimes = 1) {
+      ViewData["Message"] = "Hello" + name;
+      ViewData["NumTimes"] = numtimes;
+      return View();
+    }
+  }
+}
